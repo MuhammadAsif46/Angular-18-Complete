0fba778fc5ceb35011f9eabb5dc3969fe9c0ec0a
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-08-21 18:33:25 +0500

    added switch case

--- 7.Control-flow-statement/src/app/app.routes.ts
+++ 7.Control-flow-statement/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { IfelseComponent } from '../components/ifelse/ifelse.component';
 import { ForComponent } from '../components/for/for.component';
+import { SwitchComponent } from '../components/switch/switch.component';
 
 export const routes: Routes = [
   // create all Routes:
@@ -15,5 +16,10 @@ export const routes: Routes = [
   {
     path: 'for', // Mandatory
     component: ForComponent, // Mandatory
+  },
+  // switch route:
+  {
+    path: 'switch', // Mandatory
+    component: SwitchComponent, // Mandatory
   },
 ];
