e18dc0a1153830db490adaab44781557dfe32ffe
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-08-21 12:16:49 +0500

    creating routes in angular

--- 6.Angular-Routing/src/app/app.routes.ts
+++ 6.Angular-Routing/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from '../components/profile/profile.component';
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
+import { AboutComponent } from '../components/about/about.component';
+import { HomeComponent } from '../components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // create Profile Route
+  {
+    path: 'profile', // Mandatory
+    component: ProfileComponent, // Mandatory
+  },
+  {
+    path: 'login', // Mandatory
+    component: LoginComponent, // Mandatory
+  },
+  {
+    path: 'signup', // Mandatory
+    component: SignupComponent, // Mandatory
+  },
+  {
+    path: 'about', // Mandatory
+    component: AboutComponent, // Mandatory
+  },
+  {
+    path: 'home', // Mandatory
+    component: HomeComponent, // Mandatory
+  },
+];
