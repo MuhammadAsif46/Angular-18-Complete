7563bca799b9e1e2f6608438c6e4e0a485f57fca
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-08-21 18:06:39 +0500

    added works in @for loop in Angular

--- 7.Control-flow-statement/src/app/app.routes.ts
+++ 7.Control-flow-statement/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { IfelseComponent } from '../components/ifelse/ifelse.component';
+import { ForComponent } from '../components/for/for.component';
 
 export const routes: Routes = [
   // create all Routes:
@@ -9,5 +10,10 @@ export const routes: Routes = [
   {
     path: 'ifelse', // Mandatory
     component: IfelseComponent, // Mandatory
+  },
+  // for route:
+  {
+    path: 'for', // Mandatory
+    component: ForComponent, // Mandatory
   },
 ];
