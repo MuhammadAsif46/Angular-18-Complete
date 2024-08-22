021f1d323d8d1df6ce78e3621a618292eab2ee31
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-08-22 12:52:01 +0500

    added pipe with customs

--- 8.Pipe-In-Angular/src/component/pipe/pipe.component.ts
+++ 8.Pipe-In-Angular/src/component/pipe/pipe.component.ts
@@ -1,19 +1,28 @@
-import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { DatePipe, JsonPipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { Data } from '@angular/router';
+import { NaPipe } from '../../app/pipes/na.pipe';
 
 @Component({
   selector: 'app-pipe',
   standalone: true,
-  imports: [UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe],
+  imports: [UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,JsonPipe,NaPipe],
   templateUrl: './pipe.component.html',
   styleUrl: './pipe.component.css'
 })
 export class PipeComponent {
 
-  firstName: string = "Angular"
+  firstName: string = "Angular" 
   info: string = "This is a new session"
 
   currentDate: Date =  new Date() 
+
+  studentList: any= {
+    name: "John Doe",
+    age: 25,
+    grade: "A",
+    city:""
+    // city:"Karachi"
+  }
 
 }
