65c780e8405ac9d32bd7d428991b4f93c8a42eb0
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-08-29 18:07:16 +0500

    added reactive form in Angular

--- 9.Template-Form/src/app/app.component.ts
+++ 9.Template-Form/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { TemplateFormComponent } from "./components/template-form/template-form.component";
+import { ReactiveFormComponent } from "./components/reactive-form/reactive-form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TemplateFormComponent],
+  imports: [RouterOutlet,ReactiveFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
