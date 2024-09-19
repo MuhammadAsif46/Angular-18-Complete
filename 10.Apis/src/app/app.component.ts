7cd353349ca30a94753b3ff2e1278e2f78d51def
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-09-19 13:24:43 +0500

    create post-api compoent

--- 10.Apis/src/app/app.component.ts
+++ 10.Apis/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GetApiComponent } from "./components/get-api/get-api.component";
+import { PostApiComponent } from "./components/post-api/post-api.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GetApiComponent],
+  imports: [RouterOutlet, GetApiComponent, PostApiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
