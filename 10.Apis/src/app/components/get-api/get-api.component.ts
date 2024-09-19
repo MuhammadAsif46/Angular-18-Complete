01e5ed25d66382bac13468773641fae5b468e178
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-09-19 13:05:43 +0500

    added Api integration in Angular 18 get method

---
+++ 10.Apis/src/app/components/get-api/get-api.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-get-api',
+  standalone: true,
+  imports: [],
+  templateUrl: './get-api.component.html',
+  styleUrl: './get-api.component.css',
+})
+export class GetApiComponent {
+  userList: any[] = [];
+  customerList: any[] = [];
+
+  constructor(private http: HttpClient) {
+    // this.getAllUsers(); page load the API call will be made.
+    this.getAllCustomers();  // page load the API call will be made.
+  }
+
+  getAllUsers() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((response: any) => {
+        console.log('response-->', response);
+        this.userList = response;
+      });
+  }
+
+  getAllCustomers() {
+    this.http
+      .get('https://projectapi.gerasim.in/api/RealEstate/GetAllCustomers1')
+      .subscribe((response: any) => {
+        console.log('CustomerRes-->', response.data);
+        this.customerList = response.data;
+
+        // this.userList = response;
+      },error => {
+        console.log('Customer Error-->', error);
+        console.log('Customer Error-->', error.message);
+        // this.customerList = [];
+      });
+  }
+}
