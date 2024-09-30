e915d226c3a28ad391ecab928480162c29ca85f0
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-09-30 17:13:54 +0500

    added @Output concept in angular

--- 12.Input-Output-with-Reusable-Component/src/app/components/signup/signup.component.ts
+++ 12.Input-Output-with-Reusable-Component/src/app/components/signup/signup.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { AlertComponent } from "../../reusableComponents/alert/alert.component";
+import { CustomBtnComponent } from "../../reusableComponents/custom-btn/custom-btn.component";
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [AlertComponent],
+  imports: [AlertComponent, CustomBtnComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+
+  getData(data: any) {
+    console.log("data-->", data);
+    
+  }
 
 }
--- 12.Input-Output-with-Reusable-Component/src/app/reusableComponents/custom-btn/custom-btn.component.ts
+++ 12.Input-Output-with-Reusable-Component/src/app/reusableComponents/custom-btn/custom-btn.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-custom-btn',
   standalone: true,
   imports: [],
   templateUrl: './custom-btn.component.html',
-  styleUrl: './custom-btn.component.css'
+  styleUrl: './custom-btn.component.css',
 })
 export class CustomBtnComponent {
+  @Input() btnText: string = '';
+  @Input() btnClass: string = '';
 
+  @Output() onBtnClick = new EventEmitter<any>()
+  onClick() {
+    this.onBtnClick.emit("Hii i am from child")
+  }
 }
