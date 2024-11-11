a70ca65475a118825e1d65cf8551db19a4c081db
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-11-11 19:04:24 +0500

    added class & interface and signal documentes

--- 19.Class-&-Interface/src/app/app.component.ts
+++ 19.Class-&-Interface/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { LayoutComponent } from './components/layout/layout.component';
+// import { LoginComponent } from './components/login/login.component';
+// import { LayoutComponent } from './components/layout/layout.component';
+import { GetApiComponent } from './components/get-api/get-api.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent,LayoutComponent],
+  imports: [RouterOutlet,GetApiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
